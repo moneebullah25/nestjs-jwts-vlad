2c9e6c17b83295bbb8ac92f5f17420ce19916255
Muneeb Ullah <moneebullah25@gmail.com>
2023-08-24 19:37:04 +0500

    make some changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { AtGuard } from './common/guards';
 import { PrismaModule } from './prisma/prisma.module';
+import { AddCategoryModule } from './add-category/add-category.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule, AddCategoryModule],
   providers: [
     {
       provide: APP_GUARD,
